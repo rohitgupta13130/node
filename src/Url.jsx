3cb42f54ac901db655040bc4ff2220fd47b728f7
Shadab Alam <shadabalamcse0@gmail.com>
2024-05-24 16:47:38 +0530

    Input  Url  add

---
+++ src/Url.jsx
@@ -0,0 +1,9 @@
+export default function Url() {
+    return (
+      <>
+        <label>
+           <input name="myInput" placeholder="link from youtube or any other channels"/>
+        </label>
+        </>
+    )
+}
