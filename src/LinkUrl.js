ec42807525af2f6624f1407b0cf65787b1da01a8
Shadab Alam <shadabalamcse0@gmail.com>
2024-05-28 18:09:58 +0530

    New Changesss

--- src/LinkUrl.js
+++ src/LinkUrl.js
@@ -2,9 +2,10 @@ export default function LinkUrl() {
     return (
       <div>
        
+       
            <input name="myInput" placeholder="link from youtube or any other channels"/>
        
-       
+
       </div>
     )
 }
